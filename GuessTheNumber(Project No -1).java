e1bf698f621b1c2f308bf1642ace2c7dd0a59a8a
Ni1rmalya <113786697+Ni1rmalya@users.noreply.github.com>
2024-06-21 13:04:54 +0530

    Update GuessTheNumber(Project No -1).java

--- GuessTheNumber(Project No -1).java
+++ GuessTheNumber(Project No -1).java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
-class GuessTheNumber {
+public class GuessTheNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
